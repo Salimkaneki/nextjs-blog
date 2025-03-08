a4a200f48f366baeb364b355d44cd1c32bc9b740
Salimkaneki <salimpereira01@gmail.com>
2025-03-08 09:10:03 +0000

    learning about .module.css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,22 @@
 import Head from "next/head";
 import Link from "next/link";
 import Layout from "../../components/layout";
+import Script from 'next/script';
 
 export default function FirstPost() {
     return (
         <Layout>
             <Head>
                 <title>First Post</title>
+                <Script
+                    src="https://connect.facebook.net/en_US/sdk.js"
+                    strategy="lazyOnload"
+                    onLoad={() =>
+                    console.log(`script loaded correctly, window.FB has been populated`)
+                    }
+                />
             </Head>
+
             <h1>First Post</h1>
             <h2>
                 <Link href="/">← Back to home</Link>
